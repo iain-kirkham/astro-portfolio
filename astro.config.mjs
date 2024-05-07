34d738f6836402f08f0a111dc22c4d81c61d613c
Iain <iain.kirkham@outlook.com>
2024-05-07 21:21:02 +0100

    feat: added mdx, code highlight support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import expressiveCode from "astro-expressive-code";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://iainkirkham.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), expressiveCode(), mdx()]
 });
---
+++ src/data/socialmedia.ts
@@ -0,0 +1,6 @@
+export const socialmedia = {
+  twitter: "https://twitter.com/iainkirkham97",
+  github: "https://github.com/iain-kirkham",
+  linkedin: "https://www.linkedin.com/in/iain-kirkham/",
+  email: "mailto:iain.kirkham@outlook.com",
+};
