f948860c455d4c10feac7ca406971b484612dc1e
Iain <iain.kirkham@outlook.com>
2024-05-08 22:10:18 +0100

    feat: added auto-links, tailwind prose, styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+
 import tailwind from "@astrojs/tailwind";
 import expressiveCode from "astro-expressive-code";
-
 import mdx from "@astrojs/mdx";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://iainkirkham.dev",
   integrations: [tailwind(), expressiveCode(), mdx()],
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { properties: { class: ["anchor"] } }],
+    ],
+  },
 });
