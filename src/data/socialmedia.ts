55de2d8142c7c87aaab672ac46a2ccc42e6c4357
Iain <iain.kirkham@outlook.com>
2024-05-09 14:57:19 +0100

    feat: footer socials added

--- src/data/socialmedia.ts
+++
@@ -1,6 +0,0 @@
-export const socialmedia = {
-  twitter: "https://twitter.com/iainkirkham97",
-  github: "https://github.com/iain-kirkham",
-  linkedin: "https://www.linkedin.com/in/iain-kirkham/",
-  email: "mailto:iain.kirkham@outlook.com",
-};
--- src/data/utils.ts
+++ src/data/utils.ts
@@ -4,3 +4,10 @@ export const dateOptions = {
   month: "long",
   day: "numeric",
 };
+
+export const socials = {
+  GitHub: "https://github.com/iain-kirkham",
+  Twitter: "https://twitter.com/iainkirkham97",
+  LinkedIn: "https://www.linkedin.com/in/iain-kirkham/",
+  Email: "mailto:iain.kirkham@outlook.com",
+};
