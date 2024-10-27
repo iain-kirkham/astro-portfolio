6b107e1396965d180954d11354dcf1ea2d626cc1
Iain <iain.kirkham@outlook.com>
2024-10-27 23:53:25 +0000

    feat: added biomejs, and removed tailwindCSS classes

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -1,18 +1,18 @@
 export const dateOptions = {
-  weekday: "short",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
+	weekday: "short",
+	year: "numeric",
+	month: "long",
+	day: "numeric",
 };
 
 export const socials = {
-  GitHub: "https://github.com/iain-kirkham",
-  Twitter: "https://twitter.com/iainkirkham97",
-  LinkedIn: "https://www.linkedin.com/in/iain-kirkham/",
-  Email: "mailto:iain.kirkham@outlook.com",
+	GitHub: "https://github.com/iain-kirkham",
+	Twitter: "https://twitter.com/iainkirkham97",
+	LinkedIn: "https://www.linkedin.com/in/iain-kirkham/",
+	Email: "mailto:iain.kirkham@outlook.com",
 };
 
 export const SITE = {
-  title: " | iainkirkham.dev",
-  description: "Welcome to my new website",
+	title: " | iainkirkham.dev",
+	description: "Welcome to my new website",
 };
--- src/utils/data.utils.ts
+++ src/utils/data.utils.ts
@@ -1,5 +1,5 @@
 export function sortByDateDesc(posts: any) {
-  return posts.sort(
-    (a: any, b: any) => b.data.pubDate.getTime() - a.data.pubDate.getTime()
-  );
+	return posts.sort(
+		(a: any, b: any) => b.data.pubDate.getTime() - a.data.pubDate.getTime(),
+	);
 }
