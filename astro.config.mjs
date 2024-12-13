9dc0822ba7873ef79ef4fe0783aab72f6a4b864a
Iain <iain.kirkham@outlook.com>
2024-12-13 21:48:42 +0000

    chore: updated astro to v5 using legacy content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,52 @@
-import { defineConfig } from "astro/config";
-import { FontaineTransform } from "fontaine";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
-import tailwind from "@astrojs/tailwind";
-import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
+import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
 import playformInline from "@playform/inline";
+import expressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
+import { FontaineTransform } from "fontaine";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [
-      FontaineTransform.vite({
-        fallbacks: ["Arial"],
-        resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
-      }),
-    ],
-  },
-  prefetch: true,
-  site: "https://iainkirkham.dev",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    expressiveCode(),
-    mdx(),
-    playformInline(),
-    playformCompress(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [
-      rehypeHeadingIds,
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            class: ["anchor"],
-          },
-        },
-      ],
-    ],
-  },
+	legacy: {
+		collections: true,
+	},
+	vite: {
+		plugins: [
+			FontaineTransform.vite({
+				fallbacks: ["Arial"],
+				resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
+			}),
+		],
+	},
+	prefetch: true,
+	site: "https://iainkirkham.dev",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		expressiveCode(),
+		mdx(),
+		playformInline(),
+		playformCompress(),
+	],
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		rehypePlugins: [
+			rehypeHeadingIds,
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					properties: {
+						class: ["anchor"],
+					},
+				},
+			],
+		],
+	},
 });
